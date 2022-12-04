5d4c40f604ed8e653996fd84514c996390e237f9
maxplayerone <antekmajner@gmail.com>
2022-12-04 17:12:01 +0100

    triangle has different colours at each side

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,28 @@ use std::ptr;
 use std::str;
 
 static VS_SRC: &'static str = "
-        #version 150
-        in vec3 position;
+        #version 330
+        layout (location=0) in vec3 position;
+        layout (location=1) in vec3 a_color;
+
+        out vec3 f_color;
 
         void main(){
+            f_color = a_color;
             gl_Position = vec4(position, 1.0);
         }
 ";
 
 static FS_SRC: &'static str = "
-#version 150
+#version 330
 out vec4 out_color;
+
+in vec3 f_color;
+
 void main() {
-    out_color = vec4(1.0, 1.0, 1.0, 1.0);
+    out_color = vec4(f_color, 1.0);
 }";
 
-static FS_SRC_TWO: &'static str = "
-#version 150
-out vec4 out_color;
-void main() {
-    out_color = vec4(0.94, 1, 0.28, 1.0);
-}";
 
 fn compile_shader(src: &str, ty: GLenum) -> GLuint {
     let shader : u32;
@@ -112,79 +113,64 @@ fn main() {
     // Load the OpenGL function pointers
     gl::load_with(|symbol| gl_window.get_proc_address(symbol));
 
-    let vertices_one : [f32; 9] = [
-        -0.75, -0.5, 0.0,
-        -0.25, -0.5, 0.0,
-        -0.5, 0.0, 0.0,
+    let vertices : [f32; 18] = [
+        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+        0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+        0.0, 0.5, 0.0, 0.0, 0.0, 1.0,
     ];
-
-    let vertices_two : [f32; 9] = [
-        0.25, -0.5, 0.0,
-        0.75, -0.5, 0.0,
-        0.5, 0.0, 0.0,
-    ];
-    
 
     let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
     let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
     let program = link_program(vs, fs);
 
-    let fs2 = compile_shader(FS_SRC_TWO, gl::FRAGMENT_SHADER);
-    let program2 = link_program(vs, fs2);
+    let mut vbo = 0;
+    let mut vao = 0;
 
-    let mut vbos: [u32; 2] = [0, 0];
-    let mut vaos: [u32; 2] = [0, 0];
-
-    //to-do the first vao doesn't render the triangle
-    //when we initialize the second vbo and vaio
+    let uniformLocation;
 
     unsafe{
-        gl::GenVertexArrays(2, &mut vaos[0]);
-        gl::GenBuffers(2, &mut vbos[0]);
+        //vertex array object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
 
-        gl::BindVertexArray(vaos[0]);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbos[0]);
+        //vertex buffer
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
         gl::BufferData(
             gl::ARRAY_BUFFER, 
-            (9 * mem::size_of::<GLfloat>()) as GLsizeiptr,
-            mem::transmute(&vertices_one[0]), 
+            (18 * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            mem::transmute(&vertices[0]), 
             gl::STATIC_DRAW);
-        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
-        gl::EnableVertexAttribArray(pos_attr as GLuint);
-        gl::VertexAttribPointer(
-            pos_attr as GLuint,
-            3,
-            gl::FLOAT,
-            gl::FALSE as GLboolean,
-            0,
-            ptr::null(),
-        );
-
-        gl::BindVertexArray(vaos[1]);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbos[1]);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (9 * mem::size_of::<GLfloat>()) as GLsizeiptr,
-            mem::transmute(&vertices_two[0]),
-            gl::STATIC_DRAW,
-        );
-        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
-        gl::EnableVertexAttribArray(pos_attr as GLuint);
-        gl::VertexAttribPointer(
-            pos_attr as GLuint,
-            3,
-            gl::FLOAT,
-            gl::FALSE as GLboolean,
-            0,
-            ptr::null(),
-        );
 
         //shader program
         gl::UseProgram(program);
         gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
 
         //define the vertex buffer data
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE as GLboolean,
+            (6 * mem::size_of::<GLfloat>()).try_into().unwrap(),
+            ptr::null(),
+        );
+
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(
+            1, //location
+            3, //amount of data
+            gl::FLOAT, //type of data
+            gl::FALSE as GLboolean, //should the data be normalized (changed to -1 to 1) values?
+            (6 * mem::size_of::<GLfloat>()).try_into().unwrap(), //how long (in bytes) is the amount of data
+            mem::transmute(3 * mem::size_of::<GLfloat>()) 
+        );
+
+        uniformLocation = gl::GetUniformLocation(program, CString::new("u_color").unwrap().as_ptr());
     }
+
+    //to-do: add a uniform that will change the colour of the triangle gradually
 
     event_loop.run(move |event, _, control_flow| {
         use glutin::event::{Event, WindowEvent};
@@ -200,8 +186,8 @@ fn main() {
                         gl::DeleteProgram(program);
                         gl::DeleteShader(fs);
                         gl::DeleteShader(vs);
-                        gl::DeleteBuffers(2, &vbos[0]);
-                        gl::DeleteVertexArrays(2, &vaos[1]);
+                        gl::DeleteBuffers(1, &vbo);
+                        gl::DeleteVertexArrays(1, &vao);
                     }
                     *control_flow = ControlFlow::Exit
                 },
@@ -214,14 +200,8 @@ fn main() {
                     gl::ClearColor(0.4, 0.7, 0.3, 1.0);
                     gl::Clear(gl::COLOR_BUFFER_BIT);
 
-                    gl::BindVertexArray(vaos[0]);
                     gl::UseProgram(program);
-                    gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
-                    gl::DrawArrays(gl::TRIANGLES, 0, 3);
-
-                    gl::BindVertexArray(vaos[1]);
-                    gl::UseProgram(program2);
-                    gl::BindFragDataLocation(program2, 0, CString::new("out_color").unwrap().as_ptr());
+                    gl::Uniform4f(uniformLocation, 0.7, 0.4, 0.2, 1.0);
                     gl::DrawArrays(gl::TRIANGLES, 0, 3);
                 }
                 gl_window.swap_buffers().unwrap();
